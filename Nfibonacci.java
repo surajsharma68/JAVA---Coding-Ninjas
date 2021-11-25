fb99d5af7c719a12e255a3a3a45e53b3320adb0c
Suraj Sharma <95013741+surajsharma68@users.noreply.github.com>
2021-11-25 20:09:49 +0530

    Add files via upload

---
+++ Nfibonacci.java
@@ -0,0 +1,20 @@
+import jav.util.Scanner;
+public class Nfibonacci
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		int n1 = 0;
+		int n2 = 1;
+		
+		for(int i =1;i<=n;i++)
+		{
+		int n3=n1+n2;
+		n1=n2;
+		n2=n3;
+	    }
+		System.out.println(n1);
+	}
+}
