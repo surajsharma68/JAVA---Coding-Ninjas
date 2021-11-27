6d1847a0c308e5d0aaef7be49e5d77c7a304d6c1
Suraj Sharma <95013741+surajsharma68@users.noreply.github.com>
2021-11-27 18:35:00 +0530

    Add files via upload

---
+++ Squareroot.java
@@ -0,0 +1,25 @@
+package file4;
+import java.util.Scanner;
+public class Squareroot {
+		
+	
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int i =1;
+				while(i*i<=n)
+				{
+					i++;
+					}
+				//if (i*i!=n)
+				i = i-1;
+					
+				System.out.println(i);
+				}
+				
+		
+	}
+
+
+
