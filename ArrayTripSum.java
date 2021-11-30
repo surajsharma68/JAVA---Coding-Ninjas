58d0d3340ba3011d18f969cf03e452073fab30d3
Suraj Sharma <95013741+surajsharma68@users.noreply.github.com>
2021-11-30 11:01:35 +0530

    Update ArrayTripSum.java

--- ArrayTripSum.java
+++ ArrayTripSum.java
@@ -1,4 +1,23 @@
-package array;
+//You have been given a random integer array/list(ARR) and a number X. Find and return the triplet(s) in the array/list which sum to X.
+
+ //Note :
+ //Given array/list can contain duplicate elements.
+
+// Input format : 
+//  The first line contains an Integer 't' which denotes the number of test cases or queries to be run. Then the test cases follow.
+
+//First line of each test case or query contains an integer 'N' representing the size of the first array/list.
+
+//Second line contains 'N' single space separated integers representing the elements in the array/list.
+
+//Third line contains an integer 'X'.
+
+//Output format : 
+//For each test case, print the total number of triplets present in the array/list.
+
+//Output for every test case will be printed in a separate line.
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 import java.util.Scanner;
 
